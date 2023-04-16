d243ff57603c9302fbf2923c164d402be7ede498
vanessaneves-dev <vanessaneves.dev@gmaill.com>
2023-04-16 10:18:43 -0300

    :recycle: refactor: Alterand a Array de alunos, para conformizar com a próxima atividade

--- alunos.js
+++ alunos.js
@@ -1,15 +1,15 @@
 // alunos.js
 const alunos = [
-  { id: 1, nome: 'Vanessa Neves', media: 10.0 },
-  { id: 8, nome: 'Laura Xavier', media: 9.0 },
-  { id: 2, nome: 'Mariana Oliveira', media: 7.0 },
-  { id: 3, nome: 'Ana Pereira', media: 6.0 },
-  { id: 4, nome: 'Pedro Souza', media: 5.5 },
-  { id: 5, nome: 'Lucas Carvalho', media: 7.8 },
-  { id: 6, nome: 'Carolina Santos', media: 9.5 },
-  { id: 7, nome: 'Miguel Rocha', media: 4.0 },
-  { id: 9, nome: 'Bruno Almeida', media: 7.5 },
-  { id: 10, nome: 'Isabella Costa', media: 8.7 },
+  { nome: 'Vanessa Neves', matricula: '001', media: 10.0 },
+  { nome: 'Laura Xavier', matricula: '002', media: 9.0 },
+  { nome: 'Mariana Oliveira', matricula: '003', media: 7.0 },
+  { nome: 'Ana Pereira', matricula: '004', media: 6.0 },
+  { nome: 'Pedro Souza', matricula: '005', media: 5.5 },
+  { nome: 'Lucas Carvalho', matricula: '006', media: 7.8 },
+  { nome: 'Carolina Santos', matricula: '007', media: 9.5 },
+  { nome: 'Miguel Rocha', matricula: '008', media: 4.0 },
+  { nome: 'Bruno Almeida', matricula: '009', media: 7.5 },
+  { nome: 'Isabella Costa', matricula: '010', media: 8.7 },
 ];
 
 function filtrarPorNome(nome) {
