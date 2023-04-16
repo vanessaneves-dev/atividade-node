434a05ec4370f9e301d559f7783efc1a853f9160
vanessaneves-dev <vanessaneves.dev@gmaill.com>
2023-04-16 19:30:54 -0300

    :recycle: refactor: Passando rotas Post para arquivo alunos.js

--- alunos.js
+++ alunos.js
@@ -1,3 +1,8 @@
+const express = require("express");
+const fs = require('fs');
+
+const router = express.Router();
+
 // alunos.js
 const alunos = [
   { nome: 'Vanessa Neves', matricula: '001', media: 10.0 },
@@ -12,17 +17,85 @@ const alunos = [
   { nome: 'Isabella Costa', matricula: '010', media: 8.7 },
 ];
 
-function filtrarPorNome(nome) {
-  return alunos.filter((aluno) => aluno.nome.includes(nome));
 
+function salvarAlunos(alunos) {
+  const data = JSON.stringify(alunos, null, 2);
+  fs.writeFile('db.json', data, (err) => {
+    if (err) {
+      console.error('Erro ao salvar os dados dos alunos:', err);
+    } else {
+      console.log('Dados dos alunos salvos com sucesso em db.json');
+    }
+  });
 }
 
-function filtrarPorMedia(mediaMinima) {
-  return alunos.filter((aluno) => aluno.media >= mediaMinima);
-}
 
-module.exports = {
-  alunos,
-  filtrarPorNome,
-  filtrarPorMedia,
-};
+
+router.post('/novo', (req, res) =>  {
+  const {nome, matricula, media} = req.body;
+
+  if (
+    typeof nome !== 'string' ||
+    typeof matricula !== 'string' ||
+    typeof media !== 'number'
+  ) {
+    return res.status(400).json({ error: 'Campos inválidos' });
+  }
+ 
+  const matriculaExistente = alunos.find((aluno) => aluno.matricula === matricula);
+  if (matriculaExistente) {
+    return res.status(400).json({ error: 'Matrícula já cadastrada' });
+  }
+
+ 
+  const novoAluno = { nome, matricula, media };
+  alunos.push(novoAluno);
+
+  salvarAlunos(alunos);  
+  return res.status(201).json(novoAluno);
+});
+
+router.post('/deletar/:index', (req, res) => {
+  const index = parseInt(req.params.index);
+
+  if (isNaN(index) || index < 0 || index >= alunos.length) {
+    return res.status(404).json({ error: 'Aluno não encontrado' });
+  }
+
+  const alunoRemovido = alunos.splice(index, 1)[0];
+
+  salvarAlunos(alunos);
+  return res.status(200).json({
+    message: 'Aluno removido com sucesso',
+    aluno: alunoRemovido
+  });
+});
+
+router.post('/atualizar/:index', (req, res) => {
+
+  const index = parseInt(req.params.index);
+  console.log(req.body)
+
+  const { nome, media } = req.body;
+
+
+  if (isNaN(index) || index < 0 || index >= alunos.length) {
+    return res.status(404).json({ error: 'Aluno não encontrado' });
+  }
+
+  if (nome) {
+    alunos[index].nome = nome;
+  }
+  
+  if (media) {
+    alunos[index].media = media;
+  }
+
+  // inclui uma atualização da matricula com base no index da atualização acima
+  alunos[index].matricula = (index + 1).toString().padStart(3, '0');
+
+  salvarAlunos(alunos);
+  return res.status(200).json(alunos[index]);
+});
+
+module.exports = {router, alunos};
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const { alunos, filtrarPorNome, filtrarPorMedia } = require('./alunos.js');
+const {router, alunos} = require('./alunos.js');
 
 app.use(express.json());
 
-function salvarAlunos(alunos) {
-  const data = JSON.stringify(alunos, null, 2);
-  fs.writeFile('db.json', data, (err) => {
-    if (err) {
-      console.error('Erro ao salvar os dados dos alunos:', err);
-    } else {
-      console.log('Dados dos alunos salvos com sucesso em db.json');
-    }
-  });
+app.use("/alunos", router);
+
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
+function filtrarPorNome(nome) {
+  return alunos.filter((aluno) => aluno.nome.includes(nome));
+
+}
+
+function filtrarPorMedia(mediaMinima) {
+  return alunos.filter((aluno) => aluno.media >= mediaMinima);
 }
 
 
@@ -34,68 +37,6 @@ app.get('/alunos', (req, res) => {
   }
 });
 
-app.post('/alunos/novo', (req, res) =>  {
-  const {nome, matricula, media} = req.body;
-
-  if (
-    typeof nome !== 'string' ||
-    typeof matricula !== 'string' ||
-    typeof media !== 'number'
-  ) {
-    return res.status(400).json({ error: 'Campos inválidos' });
-  }
- 
-  const matriculaExistente = alunos.find((aluno) => aluno.matricula === matricula);
-  if (matriculaExistente) {
-    return res.status(400).json({ error: 'Matrícula já cadastrada' });
-  }
-
- 
-  const novoAluno = { nome, matricula, media };
-  alunos.push(novoAluno);
-
-  salvarAlunos(alunos);  
-  return res.status(201).json(novoAluno);
-});
-
-app.post('/alunos/deletar/:index', (req, res) => {
-  const index = parseInt(req.params.index);
-
-  if (isNaN(index) || index < 0 || index >= alunos.length) {
-    return res.status(404).json({ error: 'Aluno não encontrado' });
-  }
-
-  const alunoRemovido = alunos.splice(index, 1)[0];
-
-  salvarAlunos(alunos);
-  return res.status(200).json({
-    message: 'Aluno removido com sucesso',
-    aluno: alunoRemovido
-  });
-});
-
-app.post('/alunos/atualizar/:index', (req, res) => {
-  const index = parseInt(req.params.index);
-  const { nome, media } = req.body;
-
-  if (isNaN(index) || index < 0 || index >= alunos.length) {
-    return res.status(404).json({ error: 'Aluno não encontrado' });
-  }
-
-  if (nome) {
-    alunos[index].nome = nome;
-  }
-  
-  if (media) {
-    alunos[index].media = media;
-  }
-
-  // inclui uma atualização da matricula com base no index da atualização acima
-  alunos[index].matricula = (index + 1).toString().padStart(3, '0');
-
-  salvarAlunos(alunos);
-  return res.status(200).json(alunos[index]);
-});
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta http://localhost:3000/");
